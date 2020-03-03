ae303c46f25274e8d5255b5863c516d02ef3f8c5
unknown <victoradelowo@yahoo.com>
2020-03-03 19:45:11 +0100

    dataphyte_ckan

---
+++ ckanext/dataphyte_theme/plugin.py
@@ -0,0 +1,13 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+class Dataphyte_ThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
+
+    '''
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config):
+        toolkit.add_template_directory(config,'templates')
+        toolkit.add_public_directory(config, 'public')
+        
